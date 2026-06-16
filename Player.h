337fbc8cc8d37ce3587fef5e4b60bfe1013b73a7
loewq <blazingclock030@gmail.com>
2026-06-16 23:36:34 +0800

    Create Player.h

---
+++ Player.h
@@ -0,0 +1,27 @@
+#ifndef PLAYER_H
+#define PLAYER_H
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+
+class Player{
+    public:
+        Player():name("Unkown"),hp(20),attackPower(5),mp(100){}//預設數值
+        string getname(){return name;}
+        void sethp(double h){hp = h;}
+        void setpower(double p){attackPower = p;}
+        void setmp(double m){mp = m;}
+        double gethp()const{return hp;}
+        double getattackpower()const{return attackPower;}
+        double getmp()const{return mp;}
+    protected:
+        std::string name;
+        double hp;
+        double attackPower;
+        double mp;//法力值
+};
+
+#endif
