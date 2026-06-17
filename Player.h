f2f4d578b1bbca89d4f8ffeab4b0ebcf754719e5
loewq <blazingclock030@gmail.com>
2026-06-17 14:42:16 +0800

    player change

--- Player.h
+++ Player.h
@@ -10,13 +10,14 @@ using namespace std;
 class Player{
     public:
         Player():name("Unkown"),hp(20),attackPower(5),mp(100){}//預設數值
+        Player(string n,double h,double a,double m):name("Unkown"),hp(h),attackPower(a),mp(m){}//預設數值
         string getname(){return name;}
         void sethp(double h){hp = h;}
         void setpower(double p){attackPower = p;}
         void setmp(double m){mp = m;}
-        double gethp()const{return hp;}
-        double getattackpower()const{return attackPower;}
-        double getmp()const{return mp;}
+        virtual double gethp()const{return hp;}
+        virtual double getattack()const{return attackPower;}
+        virtual double getmp()const{return mp;}
     protected:
         std::string name;
         double hp;
