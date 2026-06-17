df23df214f517bc2ddeb6bce74275870c33ce122
seeguagee87 <s11459018@gm2.nutn.edu.tw>
2026-06-17 21:01:24 +0800

    large modify

--- item.h
+++ item.h
@@ -7,21 +7,21 @@ using namespace std;
 
 class Item {
     public:
-        Item(string n = "預設物品", string t = "HP", double e = 20.0, int q = 1) 
+        Item(string n = "default item", string t = "HP", double e = 20.0, int q = 1) 
             : name(n), type(t), effectValue(e), quantity(q) {}
 
         void use() {
             if (isAvailable()) {
-                cout << "使用了物品【" << name << "】！" << endl;
+                cout << "use item【" << name << "】!" << endl;
                 quantity--; 
             } else {
-                cout << "【" << name << "】數量不足，無法使用！" << endl;
+                cout << "【" << name << "】not enough!" << endl;
             }
         }
 
         void showInfo() const {
-            cout << "物品名稱: " << name << " | 類型: " << type 
-                 << " | 效果數值: " << effectValue << " | 數量: " << quantity << endl;
+            cout << "item: " << name << " | type: " << type 
+                 << " | effect: " << effectValue << " | quantity: " << quantity << endl;
         }
 
         bool isAvailable() const { return quantity > 0; }
